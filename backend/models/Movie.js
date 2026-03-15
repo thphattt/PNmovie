e4d132da0dd6674ba7c243f81d60ce25e1aec259
thphattt <buithanhphat433@gmail.com>
2026-03-15 13:04:17 +0700

    fix Movie models

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema(
   {
-    _id: { type: String, requied: true },
-    title: { type: String, requied: true },
-    overview: { type: String, requied: true },
-    poster_path: { type: String, requied: true },
-    backdrop_path: { type: String, requied: true },
-    release_date: { type: String, requied: true },
+    _id: { type: String, required: true },
+    title: { type: String, required: true },
+    overview: { type: String, required: true },
+    poster_path: { type: String, required: true },
+    backdrop_path: { type: String, required: true },
+    release_date: { type: String, required: true },
     original_language: { type: String },
     tagline: { type: String },
-    genres: { type: Array, requied: true },
-    casts: { type: Array, requied: true },
-    vote_average: { type: Number, requied: true },
-    runtime: { type: Number, requied: true },
+    genres: { type: Array, required: true },
+    casts: { type: Array, required: true },
+    vote_average: { type: Number, required: true },
+    runtime: { type: Number, required: true },
   },
   { timestamps: true },
 );
