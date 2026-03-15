93b9202387fa201fcecd9da2d73cd89f16fb4017
thphattt <buithanhphat433@gmail.com>
2026-03-15 19:58:21 +0700

    implement getNowPlayingMovies controller to fetch current movies

--- backend/controllers/showController.js
+++ backend/controllers/showController.js
@@ -1,9 +1,17 @@
-
+import axios from "axios";
 
 export const getNowPlayingMovies = async (req, res) => {
-   try {
-      
-   } catch (error) {
-      
-   }
-}
+  try {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      },
+    );
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
