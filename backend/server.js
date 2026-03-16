7025591f63e8c873c16cff78deab0ca9cb622123
thphattt <buithanhphat433@gmail.com>
2026-03-16 17:20:04 +0700

    add booking and admin routes, controllers, and models for managing bookings and admin functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
-import showRouter from './routes/showRoutes.js';
-
+import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 const port = 3000;
 
-await connectDB()
+await connectDB();
 
 // middleware
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
 //API routes
-app.get('/', (req, res) => res.send('Server is live!'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
-app.use('/api/show', showRouter)
+app.get("/", (req, res) => res.send("Server is live!"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show", showRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter)
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`),
+);
