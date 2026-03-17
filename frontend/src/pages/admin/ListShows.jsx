dc1cbe765674520c68c5fb7a3b0a4f4a2e31fb27
thphattt <buithanhphat433@gmail.com>
2026-03-17 17:09:13 +0700

    feat: update ListShows component to fetch shows from API and handle user-based data loading

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -3,34 +3,32 @@ import { dummyShowsData } from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import { dateFormat } from "../../lib/dateFormat";
+import { useAppContext } from "../../context/AppContext";
 
 const ListShows = () => {
   const currency = import.meta.env.VITE_CURRENCY;
+
+  const { axios, getToken, user } = useAppContext();
+
   const [shows, setShows] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const getAllShows = async () => {
     try {
-      setShows([
-        {
-          movie: dummyShowsData[0],
-          showDateTime: "2025-06-30T02:30:00.000Z",
-          showPrice: 59,
-          occupiedSeats: {
-            A1: "user_1",
-            B1: "user_2",
-            C1: "user_3",
-          },
-        },
-      ]);
+      const { data } = await axios.get("/api/admin/all-shows", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      setShows(data.shows);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching shows:", error);
     }
   };
   useEffect(() => {
-    getAllShows();
-  }, []);
+    if (user) {
+      getAllShows();
+    }
+  }, [user]);
 
   return !loading ? (
     <div>
@@ -47,11 +45,21 @@ const ListShows = () => {
           </thead>
           <tbody className="text-sm font-light">
             {shows.map((show, index) => (
-              <tr key={index} className="border-b border-primary/10 bg-primary/5 even:bg-primary/10">
+              <tr
+                key={index}
+                className="border-b border-primary/10 bg-primary/5 even:bg-primary/10"
+              >
                 <td className="p-2 min-w-45 pl-5"> {show.movie.title} </td>
                 <td className="p-2"> {dateFormat(show.showDateTime)} </td>
-                <td className="p-2"> {Object.keys(show.occupiedSeats).length} </td>
-                <td className="p-2"> {currency} {Object.keys(show.occupiedSeats).length * show.showPrice} </td>
+                <td className="p-2">
+                  {" "}
+                  {Object.keys(show.occupiedSeats).length}{" "}
+                </td>
+                <td className="p-2">
+                  {" "}
+                  {currency}{" "}
+                  {Object.keys(show.occupiedSeats).length * show.showPrice}{" "}
+                </td>
               </tr>
             ))}
           </tbody>
