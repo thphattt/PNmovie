ca2d024b1182301eaa1758ef49c8d197b1a3c041
thphattt <buithanhphat433@gmail.com>
2026-03-18 12:30:02 +0700

    feat: update MyBookings component to fetch user bookings from API and handle loading state

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,48 +1,90 @@
-import React, { useEffect, useState } from 'react'
-import { dummyBookingData } from '../assets/assets';
-import Loading from '../components/Loading';
-import timeFormat from '../lib/timeFormat';
-import { dateFormat } from '../lib/dateFormat';
+import React, { useEffect, useState } from "react";
+import { dummyBookingData } from "../assets/assets";
+import Loading from "../components/Loading";
+import timeFormat from "../lib/timeFormat";
+import { dateFormat } from "../lib/dateFormat";
+import { useAppContext } from "../context/AppContext";
 
 const MyBookings = () => {
-  const currency = import.meta.env.VITE_CURRENCY
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const { axios, getToken, user, image_base_url } = useAppContext();
 
   const [bookings, setBookings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const getMyBookings = async () => {
-    setBookings(dummyBookingData)
-    setIsLoading(false)
-  }
+    try {
+      const { data } = await axios.get("/api/user/bookings", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      if (data.success) {
+        setBookings(data.bookings);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
   useEffect(() => {
-    getMyBookings();
-  }, [])
+    if(user){
+      getMyBookings();
+    }
+  }, [user]);
   return !isLoading ? (
-    <div className='relative px-6 md:px-16 lg:px-40 pt-32 md:pt-40 min-h-[80vh]'>
-      <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+    <div className="relative px-6 md:px-16 lg:px-40 pt-32 md:pt-40 min-h-[80vh]">
+      <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
 
       {bookings.map((item, index) => (
-        <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 
-        border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
-          <div className='flex flex-col md:flex-row'>
-            <img src={item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video 
-            h-auto object-cover object-bottom rounded'/>
-            <div className='flex flex-col p-4'>
-              <p className='text-lg font-semibold'>{item.show.movie.title}</p>
-              <p className='text-gray-400 text-sm'> {timeFormat(item.show.movie.runtime)} </p>
-              <p className='text-gray-400 text-sm mt-auto'> {dateFormat(item.show.showDateTime)} </p>
+        <div
+          key={index}
+          className="flex flex-col md:flex-row justify-between bg-primary/8 
+        border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl"
+        >
+          <div className="flex flex-col md:flex-row">
+            <img
+              src={image_base_url + item.show.movie.poster_path}
+              alt=""
+              className="md:max-w-45 aspect-video 
+            h-auto object-cover object-bottom rounded"
+            />
+            <div className="flex flex-col p-4">
+              <p className="text-lg font-semibold">{item.show.movie.title}</p>
+              <p className="text-gray-400 text-sm">
+                {" "}
+                {timeFormat(item.show.movie.runtime)}{" "}
+              </p>
+              <p className="text-gray-400 text-sm mt-auto">
+                {" "}
+                {dateFormat(item.show.showDateTime)}{" "}
+              </p>
             </div>
           </div>
 
-          <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
-            <div className='flex items-center gap-4'>
-              <p className='text-2xl font-semibold mb-3'> {currency} {item.amount } </p>
-              {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 
-              text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+          <div className="flex flex-col md:items-end md:text-right justify-between p-4">
+            <div className="flex items-center gap-4">
+              <p className="text-2xl font-semibold mb-3">
+                {" "}
+                {currency} {item.amount}{" "}
+              </p>
+              {!item.isPaid && (
+                <button
+                  className="bg-primary px-4 py-1.5 mb-3 
+              text-sm rounded-full font-medium cursor-pointer"
+                >
+                  Pay Now
+                </button>
+              )}
             </div>
-            <div className='text-sm'>
-                <p><span className='text-gray-400'>Total tickets:</span> {item.bookedSeats.length}</p>
-                <p><span className='text-gray-400'>Seat numbers:</span> {item.bookedSeats.join(', ')}</p>
+            <div className="text-sm">
+              <p>
+                <span className="text-gray-400">Total tickets:</span>{" "}
+                {item.bookedSeats.length}
+              </p>
+              <p>
+                <span className="text-gray-400">Seat numbers:</span>{" "}
+                {item.bookedSeats.join(", ")}
+              </p>
             </div>
           </div>
         </div>
@@ -50,7 +92,7 @@ const MyBookings = () => {
     </div>
   ) : (
     <Loading />
-  )
-}
+  );
+};
 
-export default MyBookings
+export default MyBookings;
