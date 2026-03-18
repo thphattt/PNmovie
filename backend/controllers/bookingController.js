2dc3ce1d8b4fbfc42165dd1c16e12deabf31da94
thphattt <buithanhphat433@gmail.com>
2026-03-18 17:41:53 +0700

    push .env.example

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
+import stripe from "stripe";
 
 // Function to check availability of selected seats for a movie
 const checkSeatsAvailability = async (showId, selectedSeats) => {
@@ -54,8 +55,37 @@ export const createBooking = async (req, res) => {
     await showData.save();
 
     // stripe gateway initialize
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
 
-    res.json({ success: true, message: "Booked successfully" });
+    // Creating line items to for Stripe
+    const line_items = [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: showData.movie.title,
+          },
+          unit_amount: Math.floor(booking.amount) * 100,
+        },
+        quantity: 1,
+      },
+    ];
+
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-bookings`,
+      cancel_url: `${origin}/my-bookings`,
+      line_items: line_items,
+      mode: "payment",
+      metadata: {
+        bookingId: booking._id.toString(),
+      },
+      expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
+    });
+
+    booking.paymentLink = session.url;
+    await booking.save();
+
+    res.json({ success: true, url: session.url });
   } catch (error) {
     console.error(error.message);
     res.json({ success: false, message: error.message });
